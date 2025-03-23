09e9f97f3f3a0adef71ea67ece3acc40175d140c
Pavel Naboka <Pavmn96@yandex.ru>
2025-03-23 22:38:04 +0300

    Dev commit...
    Need fix problem with update GET-request...

--- src/main/java/web_hibernate/controller/UsersController.java
+++ src/main/java/web_hibernate/controller/UsersController.java
@@ -1,10 +1,10 @@
 package web_hibernate.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
@@ -12,14 +12,20 @@ import java.util.Optional;
 import web_hibernate.entity.User;
 import web_hibernate.service.UserService;
 
+//TODO - над методами используй GetMapping и тд а не RequestMapping. его используй только над контроллером
+//     - имя метода getUsersList() не отвечает тому что он делает. если по id пользователь не найден - выкидывай эксепшн
+//     - prepareForUpdate() сделай ГЕТ
 @Controller
 @RequestMapping(value = {"/","/users"})
 public class UsersController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @RequestMapping(path = {"", "/", "/all"}, method = RequestMethod.GET)
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = {"", "/", "/all"})
     public String getUsersList(
             @RequestParam(name = "update_user_id", required = false) Long beingUpdateUserId,
             ModelMap modelMap) {
@@ -30,7 +36,7 @@ public class UsersController {
         return "users";
     }
 
-    @RequestMapping(path = {"/delete"}, method = RequestMethod.POST)
+    @PostMapping(path = {"/delete"})
     public String deleteUserById(
             @RequestParam(name = "id") Long id,
             ModelMap modelMap) {
@@ -38,19 +44,20 @@ public class UsersController {
         return "redirect:/users/";
     }
 
-    @RequestMapping(path = {"/read_for_update"}, method = RequestMethod.POST)
+    @GetMapping(path = {"/read_for_update"})
     public String prepareForUpdate(@RequestParam(name = "update_user_id") Long beingUpdateUserId,
                                    ModelMap modelMap) {
+        // По неизвестной причине не получает передавать RequestParam в GET-запросе
         return "redirect:/users?update_user_id=" + beingUpdateUserId;
     }
 
-    @RequestMapping(path = {"/add"}, method = RequestMethod.POST)
+    @PostMapping(path = {"/add"})
     public String addUser(User user, ModelMap modelMap) {
         userService.addUser(user);
         return "redirect:/users/";
     }
 
-    @RequestMapping(path = {"/update"}, method = RequestMethod.POST)
+    @PostMapping(path = {"/update"})
     public String updateUser(User user, ModelMap modelMap) {
         userService.updateUser(user);
         return "redirect:/users/";
