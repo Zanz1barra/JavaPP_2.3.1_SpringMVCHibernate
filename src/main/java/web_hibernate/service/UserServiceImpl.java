566f987115b0b4eb3f9e8152362fc9a6ee93c2bd
Pavel Naboka <Pavmn96@yandex.ru>
2025-03-24 18:08:12 +0300

    Add `@Transactional` annotation to `UserServiceImpl` methods that modify DB

--- src/main/java/web_hibernate/service/UserServiceImpl.java
+++ src/main/java/web_hibernate/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package web_hibernate.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,17 +30,20 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void deleteUserById(Long id) {
         Optional<User> deletingUser = getUserById(id);
         deletingUser.ifPresent(userRepository::delete);
     }
 
     @Override
+    @Transactional
     public User addUser(User user) {
         return userRepository.save(user);
     }
 
     @Override
+    @Transactional
     public User updateUser(User user) {
         return userRepository.save(user);
     }
